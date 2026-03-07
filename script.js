03b6afef6a46a843d65cb8906c84771e8a37a4da
Sajjad Hosain <sajjadhossainctg26@gmail.com>
2026-03-08 02:11:30 +0400

    fixed issue

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const loadLessons = () => {
 const displayCard = (id) => {
   fetch(`https://openapi.programming-hero.com/api/level/${id}`)
     .then((res) => res.json())
-    .then((json) => displayCardDetails(json));
+    .then((json) => {
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      remvoeActivebtn();
+      clickBtn.classList.add("active");
+      console.log(clickBtn);
+      displayCardDetails(json);
+    });
 };
-
+const remvoeActivebtn = () => {
+  const btn = document.querySelectorAll(".actremove");
+  console.log(btn);
+  btn.forEach((btn) => btn.classList.remove("active"));
+};
 const displayLesson = (lessons) => {
   const button = document.getElementById("learn-container");
   button.innerHTML = ``;
@@ -17,7 +27,7 @@ const displayLesson = (lessons) => {
     const creatDiv = document.createElement("div");
 
     creatDiv.innerHTML = `
-     <button id="lesson-btn-${lesson.level_no}" onclick="displayCard(${lesson.level_no})" class="btn btn-primary btn-outline sm:text-sm"
+     <button id="lesson-btn-${lesson.level_no}" onclick="displayCard(${lesson.level_no})" class="btn btn-primary btn-outline sm:text-sm actremove"
             ><i class="fa-solid fa-book-open"></i> ${lesson.lessonName} -${lesson.level_no}</
     button>
     `;
@@ -56,7 +66,7 @@ const displayCardDetails = (details) => {
             <p>Meaning/Pronounciation</p>
             <h1 class="text-xl font-bold">${info}/${detail.pronunciation}</h1>
             <div class="card-btn flex justify-around mt-[30px]">
-              <button class=" bg-blue-300 rounded-full p-[10px]">
+              <button onclick="my_modal_2.showModal()" class=" bg-blue-300 rounded-full p-[10px] cursor-pointer">
                 <i class="fa-solid fa-circle-info text-white"></i>
               </button>
               <button class=" bg-blue-300 rounded-full p-[10px]">
